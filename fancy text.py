0ed04598c47723a763738dee0b8cdd1cfbeb0689
Aslngsng <salangsang.abril@gmail.com>
2024-03-25 01:01:05 +0800

    made minor revision on the function asking for dream job

--- fancy text.py
+++ fancy text.py
@@ -6,14 +6,16 @@ print("Hi, " + name + " You look great today!")
 #ask for dream job
 def sharing_dream_job(): 
     answer = input("Would you like to share your dream job? ")
-    if answer.lower() in ("yes", "sure", "I would like to"):
-        job = input("That's nice! What is your dream job then? ")
-        return job
-    else:
+    if answer.lower() =="no":
         print("That's okay! You going to figure it out like you always do!")
         return None
+    else:
+        job = input("That's nice! What is your dream job then? ")
+        return job
+    
 #call the funtion
 job = sharing_dream_job()
+
 if job: 
     print("That's an amzaing job! " + name)
 
